c82abf1f2ed074e4d6dea0074a97324eecd6e8e0
cjuwuxd <cjuwuxd@gmail.com>
2025-07-10 18:30:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,49 @@
+import cv2 
 import streamlit as st
+from PIL import Image
+import numpy as np
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+def Camera():
+    global name
+    global section
+    cam_mode = st.checkbox("Enable Camera")
+    picture = st.camera_input("Attendance Check!!!" , disabled=not cam_mode)
+    
+    if picture is not None:
+        byte = picture.getvalue()
+        cv2_img =  cv2.imdecode(np.frombuffer(byte, np.uint8), cv2.IMREAD_COLOR)
+        detector = cv2.QRCodeDetector()
+
+        data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
+        st.write(data)
+        print(data)
+
+        data = data.split(", ")
+        name = data[0]
+        section = data[1]
+        attendance()
+
+
+
+def attendance():
+    url = "https://docs.google.com/forms/d/1M3fMgk_zU2X5SXNdyv8SipNehEQZeOoDJ8DoRYCjOnw/formResponse"
+
+    data = {
+        "entry.530719642" : f"{name}",   
+        "entry.1493533031" : f"{section}"
+    }
+
+    try:
+        response = requests.post(url, data=data)
+
+        print(response)
+        print(response.status_code)
+
+    except:
+        pass
+
+
+Camera()
